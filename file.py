0d0918d15b0006c3281de9486cc560466aaac1bf
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-09 16:03:07 +0100

    Diff b/w writind and appending into files

--- file.py
+++ file.py
@@ -4,3 +4,6 @@ saveFile = open('example.txt', 'w')
 
 saveFile.write(writeText);
 saveFile.close();
+
+# Writing into a file will overide any data or text that has being previously stored or present in the file
+# Appending will not overide the current text present rather it will  update the file with the current data 
