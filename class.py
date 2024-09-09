fda2298e68ab7ff0ea5aca34ed45c2501fe84ddf
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-09 17:03:41 +0100

    functions added for calculator class

---
+++ class.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def add(x,y):
+        answer = x + y
+        print(answer)
+
+    def sub (x,y):
+        answer = x - y
+        print(answer)
+    
+    def mult(x,y):
+        answer = x * y
+        print(answer)
+
+    def div(x,y):
+        answer = x/y
+        print(answer) 
+
