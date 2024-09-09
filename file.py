57ed14cf9bae0c473b7c66b3df4b442d56df1f16
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-09 15:52:15 +0100

    example.txt file created using python open() function

--- file.py
+++ file.py
@@ -1,3 +1,6 @@
 writeText = "Exampletext"
 # writing into file, if file is not present it's being  created
-saveFile = open('example')
+saveFile = open('example.txt', 'w')
+
+saveFile.write(writeText);
+saveFile.close();
