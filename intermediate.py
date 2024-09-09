589d1c67d6471b40f1ee0a64e83a13a9f80a18a0
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-09 21:44:51 +0100

    working with statistics module

--- intermediate.py
+++ intermediate.py
@@ -1,2 +1,12 @@
-name = input('what is ur name?')
-print(name)
+# name = input('what is ur name?')
+# print(name)
+
+import statistics
+
+list = [1,2,3,4,5,6,4,5,5,6,]
+
+x= statistics.mean(list)
+print(x)
+
+mode = statistics.mode(list)
+print(mode)
