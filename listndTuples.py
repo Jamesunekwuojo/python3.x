d77b49bafe02879419ab01e323aae66c8d6d543e
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-10 11:40:18 +0100

    playing with list functions

--- listndTuples.py
+++ listndTuples.py
@@ -3,6 +3,21 @@
 def example():
     return 2,4,4,5,
 
-(a,b,c,d) = example() # THis a tuple
+(a,b,c,d) = example() # This is  a tuple - it's majorly used for variable assignments sequence unparking, it's inmutable
 
-print(a)
+print(d)
+
+x = [1,2,3,4,5,6,] #Example of a list
+print(x)
+x.append(12)
+print(x)
+x.insert(5,6)
+print(x)
+x.remove(6)
+print(x)
+print(x.count(4))
+
+
+x= ['dfvd', 'cvfvsv', 'sfvfv', 'wsvfvs']
+x.sort()
+print(x)
